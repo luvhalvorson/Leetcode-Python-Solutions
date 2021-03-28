9246239aa0698bf11529c0c1b3d607c67fa078e9
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-03-28 03:24:03 +0000

    Sync LeetCode submission - Palindromic Substrings (python3)

---
+++ problems/palindromic_substrings/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        count = 0
+        start, end, maxL = 0, 0, 0
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(i):
+                dp[j][i] = (s[j] == s[i]) & ((i - j < 2) | dp[j + 1][i - 1])
+                if dp[j][i]:
+                    count += 1
+            dp[i][i] = 1
+            count += 1
+        return count
