dd36fd23d073630e88bda69bce9e3dfd25e6d124
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-07-07 07:54:25 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (python3)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
@@ -6,26 +6,17 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        # # pre : root l r
-        # # in: l root r
-        # head = TreeNode(preorder[0])
-        # p1 = 1
-        # p2 = 0
-        # leng = len(preorder)
-        # if p1 == p2:
-        #     head.left = TreeNode(preorder[p1])
-        #     p1 += 1
-        #     p2 += 2 # skip root
-        # else
-        def helper(pre_start, in_start, in_end):
-            if pre_start > len(preorder) - 1 or in_start > in_end:
-                return None
-            root = TreeNode(preorder[pre_start])
-            in_i = 0
-            for i in range(in_start, in_end + 1):
-                if inorder[i] == root.val:
-                    in_i = i
-            root.left = helper(pre_start + 1, in_start, in_i - 1)
-            root.right = helper(pre_start + in_i - in_start + 1 , in_i + 1, in_end)
-            return root
-        return helper(0, 0, len(inorder) - 1)
+        # pre: rLR
+        # in: LrR
+        if not preorder:
+            return None
+        r = TreeNode(preorder[0])
+        ri = 0
+        for i in range(len(inorder)):
+            if inorder[i] == r.val:
+                ri = i
+                break
+        r.left = self.buildTree(preorder[1:1+ri], inorder[:ri])
+        r.right = self.buildTree(preorder[1+ri:], inorder[ri+1:])
+        
+        return r
