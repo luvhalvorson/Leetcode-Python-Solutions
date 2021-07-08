7b7c7b596870f83391bbe7cd41fbf7ee339dee78
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-07-08 08:34:45 +0000

    Sync LeetCode submission - Container With Most Water (python3)

--- problems/container_with_most_water/solution.py
+++ problems/container_with_most_water/solution.py
@@ -1,41 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # sublistlen+1 * min(兩方端點)
-        # themax = 0
-        # if len(height) == 2:
-        #     return min(height)*1
-        # for sublen in range(len(height), 1, -1):
-        #     for i in range(len(height)-sublen+1):
-        #         themax = max(themax, (sublen-1)*min(height[i], height[i+sublen-1]))
-        #         #rint(themax)      
-        # return themax
-        
-        # l = 0
-        # r = len(height) -1
-        # water = 0
-        # while l < r:
-        #     water = max(water, min(height[l], height[r]) *(r-l) )
-        #     if height[l] < height[r]:
-        #         l += 1
-        #     else:
-        #         r -= 1
-        #     #print(l, r, min(height[l], height[r]) *(r-l), water)
-        # return water
+        maxwater = 0
         l = 0
         r = len(height) - 1
-        max_area = 0
-        hmax = max(height)
-        while (r - l) * hmax >= max_area :
-            if height[l] <= height[r] :
-                area = height[l] * (r-l) 
-                l = l + 1
-            elif height[l] > height[r] :
-                area = height[r] * (r-l)
-                r = r - 1
-            #else :
-            #    area = height[l] * (r-l)
-             #   l = l + 1
-                
-            if area > max_area :
-                max_area = area
-        return max_area
+        while l < r:
+            #print(l, r)
+            maxwater = max(maxwater, (r-l) * min(height[r], height[l]))
+            #print((r-l) * min(height[r], height[l]))
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxwater
