742c446cd294499f6788c22f2c47a1fdc80388a9
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-07-19 14:31:28 +0000

    Sync LeetCode submission - Reverse Linked List (python3)

--- problems/reverse_linked_list/solution.py
+++ problems/reverse_linked_list/solution.py
@@ -5,21 +5,27 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        if not head : return None
-        c = head.next
-        nnode = c
-        pnode = head
+        if not head or not head.next : return head
+        newhead = self.reverseList(head.next)
+        head.next.next = head
         head.next = None
-        while c:
-            nnode = c.next #記錄下個node
-            c.next = pnode #把next只回前面一個node
-            pnode = c #更新前一個node為現在的node
-            #self.printlist(p)
-            c = nnode #往下一個node處理
-        return pnode
-    
-    def printlist(self, head):
-        while head:
-            print(head.val,end=" ")
-            head = head.next
-        print(" ====")
+        return newhead
+        # if not head : return None
+        # c = head
+        # pnode = None
+        # while c:
+        #     temp = c.next
+        #     c.next = pnode
+        #     pnode = c
+        #     c = temp
+        # return pnode
+            
+#         if not head:
+#             return None
+#         if not head.next:
+#             return head
+#         rest = self.reverseList(head.next)
+#         head.next.next = head
+#         head.next = None
+        
+#         return rest
