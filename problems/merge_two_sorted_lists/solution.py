fa4523ec7ce8945ccd1429b60a5908d04b048a4c
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-07-25 16:20:07 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -9,26 +9,34 @@ class Solution:
             return l2
         if not l2:
             return l1
-        p1 = l1
-        p2 = l2
-        if p1.val < p2.val:
-            chead = p1 
-            p1 = p1.next
+        if l1.val <= l2.val:
+            return ListNode(l1.val, self.mergeTwoLists(l1.next, l2))
         else:
-            chead = p2
-            p2 = p2.next
-        c = chead
-        while p1 or p2:
-            if p1 and p2:
-                if p1.val < p2.val:
-                    c.next = p1
-                    p1 = p1.next
-                else:
-                    c.next = p2
-                    p2 = p2.next
-            else:
-                c.next = p1 or p2
-                break
-            c = c.next
+            return ListNode(l2.val, self.mergeTwoLists(l1, l2.next))
+#         if not l1:
+#             return l2
+#         if not l2:
+#             return l1
+#         p1 = l1
+#         p2 = l2
+#         if p1.val < p2.val:
+#             chead = p1 
+#             p1 = p1.next
+#         else:
+#             chead = p2
+#             p2 = p2.next
+#         c = chead
+#         while p1 or p2:
+#             if p1 and p2:
+#                 if p1.val < p2.val:
+#                     c.next = p1
+#                     p1 = p1.next
+#                 else:
+#                     c.next = p2
+#                     p2 = p2.next
+#             else:
+#                 c.next = p1 if p1 else p2
+#                 break
+#             c = c.next
                 
-        return chead
+#         return chead
