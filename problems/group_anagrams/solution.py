509b22404bca46b1966cee4beeba03e491a6f6f6
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-13 06:07:08 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -1,14 +1,7 @@
-from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {}
-        for str in strs:
-            mylist = [0] * 26
-            for c in str:
-                mylist[ord(c) - ord('a')] += 1
-            if tuple(mylist) in hashmap:
-                hashmap[tuple(mylist)].append(str)
-            else:
-                hashmap[tuple(mylist)] = [str]     
-        return list(hashmap.values())
-            
+        d = {}
+        for s in strs:
+            key = tuple(sorted(s))
+            d[key] = d.get(key, []) + [s]
+        return d.values()
