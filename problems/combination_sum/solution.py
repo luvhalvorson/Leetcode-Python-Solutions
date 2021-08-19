514047c6eb6c96699be6b643a950ed9376da3642
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-19 04:53:21 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,30 +1,45 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def dfs(start_i, target, path, res):
+        def dfs(cdd, target, path):
+            if target < 0:
+                return
             if target == 0:
                 res.append(path)
-            elif target < 0:
-                return
-            while start_i < len(candidates):
-                dfs(start_i, target - candidates[start_i], path + [candidates[start_i]], res)
-                start_i += 1
-        dfs(0, target, [], res)
+                return 
+            for i in range(len(cdd)):
+                dfs(cdd[i:], target - cdd[i], path + [cdd[i]])
+        
+        
+        dfs(candidates, target, [])
         return res
-        # candidates.sort()
-        # dp = [[] for _ in range(target)]
-        # for i in range(target):
-        #     #theset = set()
-        #     for j in range(len(candidates)):
-        #         if candidates[j] > i + 1:
-        #             break
-        #         #if candidates[j] in theset:
-        #         #    continue
-        #         curtarget = i + 1 - candidates[j]
-        #         #theset.add(curtarget)
-        #         if curtarget == 0:
-        #             dp[i] += [[candidates[j]]]
-        #         if curtarget > 0:
-        #             dp[i] += [ [candidates[j]] + e  for e in dp[curtarget - 1] if e[0] >= candidates[j]]
-        #     #print(dp[i])
-        # return dp[-1]
+        
+        
+        # res = []
+        # def dfs(start_i, target, path, res):
+        #     if target == 0:
+        #         res.append(path)
+        #     elif target < 0:
+        #         return
+        #     while start_i < len(candidates):
+        #         dfs(start_i, target - candidates[start_i], path + [candidates[start_i]], res)
+        #         start_i += 1
+        # dfs(0, target, [], res)
+        # return res
+        # # candidates.sort()
+        # # dp = [[] for _ in range(target)]
+        # # for i in range(target):
+        # #     #theset = set()
+        # #     for j in range(len(candidates)):
+        # #         if candidates[j] > i + 1:
+        # #             break
+        # #         #if candidates[j] in theset:
+        # #         #    continue
+        # #         curtarget = i + 1 - candidates[j]
+        # #         #theset.add(curtarget)
+        # #         if curtarget == 0:
+        # #             dp[i] += [[candidates[j]]]
+        # #         if curtarget > 0:
+        # #             dp[i] += [ [candidates[j]] + e  for e in dp[curtarget - 1] if e[0] >= candidates[j]]
+        # #     #print(dp[i])
+        # # return dp[-1]
