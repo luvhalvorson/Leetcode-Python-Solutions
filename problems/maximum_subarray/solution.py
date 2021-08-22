76cc9f20aeae4958020c6983a0036956e6c8a1a5
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-22 10:15:10 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        curmax = themax = nums[0]
+        cursum, maxsum = nums[0], nums[0]
         for num in nums[1:]:
-            curmax = max(curmax+num, num)
-            themax = max(themax, curmax)
-        return themax
+            if cursum + num > num:
+                cursum += num
+            else:
+                cursum = num
+            maxsum = max(maxsum, cursum)
+        return maxsum
+        
