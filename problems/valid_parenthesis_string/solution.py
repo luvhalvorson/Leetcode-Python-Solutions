c137eabfcecca572998457d623985ba5b032d359
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-26 01:52:12 +0000

    Sync LeetCode submission - Valid Parenthesis String (python3)

---
+++ problems/valid_parenthesis_string/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack = []
+        s_stack = []
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif c == ")":
+                if stack:
+                    stack.pop()
+                elif s_stack:
+                    s_stack.pop()
+                else:
+                    return False
+            else:
+                s_stack.append((i))
+                
+        for i in stack[::-1]:
+            if s_stack:
+                s_i = s_stack.pop()
+                if s_i <= i:
+                    return False
+            else:
+                return False
+        return True
