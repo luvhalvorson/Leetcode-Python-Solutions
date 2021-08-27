e8ac666ae281ac1318b2f5714ad70904b505000a
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-27 07:48:05 +0000

    Sync LeetCode submission - Coin Change (python3)

--- problems/coin_change/solution.py
+++ problems/coin_change/solution.py
@@ -1,10 +1,17 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        max = amount + 1
-        dp = [max] * max
-        dp[0] = 0
-        for i in range(1, amount + 1):
-            for j in range(len(coins)):
-                if coins[j] <= i:
-                    dp[i] = min(dp[i], dp[i-coins[j]] + 1)
-        return -1 if dp[amount] > amount else dp[amount]
+        
+        dp = [0] + [float('inf')] * (amount)
+        coins.sort()
+        #print(coins)
+        for i in range(1, amount+1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+                else:
+                    break
+                    # 為什麼這個break會有影響？我本來以為後面都會coin>i 那就不用多跑．．
+
+        return dp[-1] if dp[-1] <= amount else -1
+                
+                
