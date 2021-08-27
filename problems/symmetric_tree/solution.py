62c59027c19bff281023ec6824e501e364ad64a3
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-27 04:00:04 +0000

    Sync LeetCode submission - Symmetric Tree (python3)

--- problems/symmetric_tree/solution.py
+++ problems/symmetric_tree/solution.py
@@ -6,36 +6,18 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        q = [root]
-        f = 1
-        while f:
-            f = 0
-            print([n.val if n is not None else n for n in q])
-            for _ in range(len(q)):
-                cur = q.pop(0)
-                if cur:
-                    if cur.left:
-                        q.append(cur.left)
-                        # to specify that there is not None val in the queue
-                        f = 1
-                    else:
-                        q.append(None)
-                    if cur.right:
-                        q.append(cur.right)
-                        f = 1
-                    else:
-                        q.append(None)
-           
-            if len(q) % 2:
+        def ismirror(l, r):
+            if not l and not r:
+                return True
+            if not r or not l:
                 return False
-            else:
-                # check the current queue
-                for i in range(len(q) // 2):
-                    if q[i] is None and q[-(i+1)] is None:
-                        continue
-                    elif q[i] is None or q[-(i+1)] is None:
-                        return False
-                    if q[i].val != q[-(i+1)].val:
-                        return False
-        
-        return True
+            if l.val != r.val:
+                return False
+            if ismirror(l.left, r.right) and ismirror(l.right, r.left):
+                return True
+
+        if ismirror(root.left, root.right):
+            return True
+        else:
+            return False
+
