0946c0376b41c25769717056706d2c34e58d4234
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-29 14:06:17 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python3)

--- problems/longest_increasing_subsequence/solution.py
+++ problems/longest_increasing_subsequence/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        
         maxcount = 0
         dp = [1] * len(nums) 
         for i in range(len(nums)):
@@ -9,3 +10,21 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1) 
             maxcount = max(maxcount, dp[i])
         return maxcount
+#         for i, num in enumerate(nums[::-1]):               
+#             'Find leftmost value greater than x'
+#             print(resdic)
+#             first_bigger_i = bisect.bisect_right(btree, num)
+#             if first_bigger_i != len(btree):
+#                 res = 1 + resdic[btree[first_bigger_i]]                
+#                 btree.insert(first_bigger_i, num)
+#                 resdic[num] = res
+#                 maxlen = max(maxlen, res)
+#             else:        
+#             # cant find first bigger
+#             # list is empty or no nums bigger than cur
+               
+#                 resdic[num] = 1
+#                 btree.insert(len(btree), num)
+
+#         return maxlen   
+ 
