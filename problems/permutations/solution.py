621b0dc9f1a3088cb50a153f1680870752e5548f
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-08-31 11:58:20 +0000

    Sync LeetCode submission - Permutations (python3)

--- problems/permutations/solution.py
+++ problems/permutations/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1:
-            return [nums]
-        if len(nums) == 2:
-            return [nums, [nums[1], nums[0]]]  
+        # 123   1   rec([23])   2  rec(3)   
+        # 132   1        23     3    2  
+        # 213    2
+        #
         res = []
-        for i, num in enumerate(nums):
-
-            res += [ [num] + l for l in self.permute(nums[: i] + nums[i + 1:])]
+        
+        def backtrack(numlist, path):
+            
+            if len(numlist) == 1:
+                res.append(path + numlist)
+                return
+            for i in range(len(numlist)):
+                backtrack(numlist[:i] + numlist[i+1:], path + [numlist[i]])
+        backtrack(nums, [])    
+        
         return res
-    
-        # def help(listnums):
-        #     if len(listnums) == 1:
-        #         return [listnums]
-        #     if len(listnums) == 2:
-        #         return [listnums, [listnums[1], listnums[0]]]
