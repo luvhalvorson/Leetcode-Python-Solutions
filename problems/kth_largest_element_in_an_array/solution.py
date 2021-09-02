c353031041dcb71e43f91de4c78414de007725b0
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-09-02 11:50:23 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (python3)

--- problems/kth_largest_element_in_an_array/solution.py
+++ problems/kth_largest_element_in_an_array/solution.py
@@ -1,4 +1,28 @@
-
+from random import randint
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return sorted(nums, reverse=True)[k - 1]        
+        #return sorted(nums, reverse=True)[k - 1]
+       
+        
+        def partition(l, r):
+            p_i = random.randint(l, r)
+            nums[r], nums[p_i] = nums[p_i],  nums[r]            
+            j = l
+            for i in range(l, r):
+                if nums[i] > nums[r]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    j += 1
+            nums[j], nums[r] = nums[r], nums[j]
+            return j
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            
+            cur = partition(l, r)
+            
+            if  cur + 1 < k:
+                l = cur + 1
+            elif cur + 1 > k:
+                r = cur - 1
+            else:
+                return nums[cur]
+        
