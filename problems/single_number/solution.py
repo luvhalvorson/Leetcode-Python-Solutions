163eba6f955d6607b1beaae0a2dffd8d44a49c31
D LI A IV <U10516040@go.utaipei.edu.tw>
2021-09-20 11:33:39 +0000

    Sync LeetCode submission - Single Number (python3)

--- problems/single_number/solution.py
+++ problems/single_number/solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dic = {}
+        res = 0
         for num in nums:
-            if num not in dic:
-                dic[num] = 0
-            else:
-                dic.pop(num)
-        res = [key for key in dic.keys()]
-        return res[0]
+            res ^= num
+        return res
